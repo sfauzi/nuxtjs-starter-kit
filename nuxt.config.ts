7175bfa0691e39f47437d8bc22bab267f0f35a58
sfauzi <hello@sfauzi.dev>
2026-05-04 16:28:46 +0700

    style: :lipstick: setup tailwindcss and google fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,20 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-
+  css: ['~/assets/css/main.css'],
   modules: [
     '@nuxt/fonts',
     '@nuxt/icon',
     '@nuxt/image',
     '@nuxtjs/google-fonts',
     '@nuxtjs/tailwindcss'
-  ]
+  ],
+  googleFonts: {
+    families: {
+      Manrope: true,
+      Mansalva: true,
+      Rubik: true,
+      "DM Sans": true,
+    },
+  },
 })
